5b8322cbc1e17bc77144f0e645eb96888d904044
OrsiniBr <udochukwugodwin13@gmail.com>
2025-12-08 19:49:54 +0100

    royalty address, abi added

--- contracts/DerivativeFactory.sol
+++ contracts/DerivativeFactory.sol
@@ -27,6 +27,7 @@ interface IStoryDerivative {
 
 interface ILyricToken {
     function transfer(address to, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
     function balanceOf(address account) external view returns (uint256);
 }
 
